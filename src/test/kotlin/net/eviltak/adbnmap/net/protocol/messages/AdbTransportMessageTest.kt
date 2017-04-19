15f61939fca0d42ec0c767431b4f735cec741601
Arav Singhal <singhal.arav@gmail.com>
2017-04-19 11:05:34 +0530

    Add copyright notice to each file

--- src/test/kotlin/net/eviltak/adbnmap/net/protocol/messages/AdbTransportMessageTest.kt
+++ src/test/kotlin/net/eviltak/adbnmap/net/protocol/messages/AdbTransportMessageTest.kt
@@ -1,3 +1,20 @@
+/*
+ * adb-nmap: An ADB network device discovery and connection library
+ * Copyright (C) 2017-present Arav Singhal and adb-nmap contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * The full license can be found in LICENSE.md.
+ */
+
 package net.eviltak.adbnmap.net.protocol.messages
 
 class AdbTransportMessageTest {
