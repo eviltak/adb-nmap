e1b63e4f776a49ec77b767bad410641b393a46c0
Arav Singhal <singhal.arav@gmail.com>
2017-05-01 13:55:01 +0530

    Delete NetworkAddress, Refactor and document NetworkMapper

--- src/main/kotlin/net/eviltak/adbnmap/net/NetworkMapper.kt
+++ src/main/kotlin/net/eviltak/adbnmap/net/NetworkMapper.kt
@@ -17,27 +17,50 @@
 
 package net.eviltak.adbnmap.net
 
+import net.eviltak.adbnmap.net.protocol.Protocol
 import java.net.Socket
+import java.net.SocketAddress
 
-typealias DeviceValidator = (Socket) -> Boolean
-
-class NetworkMapper(val validateDevice: DeviceValidator) {
-    fun pingDevice(address: NetworkAddress): Boolean {
+/**
+ * Scans networks for devices which use the specified protocol.
+ *
+ * @param P The type of protocol devices are expected to use.
+ *
+ * @property protocolFromSocket Creates an instance of the protocol of type [P] from the socket to be used
+ * to connect to the host.
+ */
+class NetworkMapper<out P: Protocol<*>>(val protocolFromSocket: (Socket) -> P) {
+    /**
+     * Pings the host at [socketAddress] if such a host exists to check whether it supports the
+     * specified protocol.
+     *
+     * @param socketAddress The socket address of the host to ping.
+     * @return True if the host at [socketAddress] exists and supports the specified protocol.
+     */
+    fun ping(socketAddress: SocketAddress): Boolean {
         return Socket().use {
             try {
                 // TODO: Refactor timeout to config class
-                it.connect(address.toSocketAddress(), 500)
+                it.connect(socketAddress, 500)
+                val protocol = protocolFromSocket(it)
+                protocol.hostUsesProtocol()
             }
             catch(_: Exception) {
-                return@use false
+                false
             }
-
-            return@use validateDevice(it)
         }
     }
 
-    fun retrieveDevicesFrom(addresses: Iterable<NetworkAddress>): List<NetworkAddress> {
-        return addresses.filter { pingDevice(it) }
+    /**
+     * Pings all hosts in [socketAddresses] and returns the address of all devices that support the
+     * protocol [P].
+     *
+     * @param socketAddresses The addresses to check.
+     * @return A [List] containing the addresses of all devices in [socketAddresses] that support the
+     * protocol [P].
+     */
+    fun filterDevices(socketAddresses: Iterable<SocketAddress>): List<SocketAddress> {
+        return socketAddresses.filter { ping(it) }
     }
 }
 
